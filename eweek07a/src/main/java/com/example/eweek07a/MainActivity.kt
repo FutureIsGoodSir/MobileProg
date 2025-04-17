824b13524bb3bc9c6d322a5acf983f36e79cb391
explodeTheComputer <dnfjddk2@gmail.com>
2025-04-17 18:35:06 +0900

    네이게이션 실습 완료

---
+++ eweek07a/src/main/java/com/example/eweek07a/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.eweek07a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eweek07a.example01.uicomponents.MainScreen
+import com.example.eweek07a.ui.theme.MainScreenktTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainScreenktTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainScreenktTheme {
+        Greeting("Android")
+    }
+}
